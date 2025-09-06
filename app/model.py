65d2284b8b6f11b9ebe8daa4e9c2cdd2b319e91f
Salman Fariz Navas <46594737+SalmanFarizN@users.noreply.github.com>
2025-09-06 08:48:18 +0000

    Refactor model.py to implement model architecture retrieval and loading functions

--- app/model.py
+++ app/model.py
@@ -3,6 +3,10 @@ import wandb
 
 # This gives us access to the variables set in the .env file
 from loadotenv import load_env
+from torchvision.models import resnet18, ResNet
+from torch import nn
+from pathlib import Path
+import torch
 
 load_env()
 wandb_api_key = os.environ.get("WANDB_API_KEY")
@@ -14,6 +18,7 @@ os.makedirs(MODELS_DIR, exist_ok=True)
 
 
 def download_artifact():
+    """Download the model weights from Weights & Biases."""
     assert (
         "WANDB_API_KEY" in os.environ
     ), "WANDB_API_KEY not found in environment variables. Please set it in the .env file."
@@ -35,3 +40,27 @@ def download_artifact():
 
 
 download_artifact()
+
+
+def get_raw_model() -> ResNet:
+    """Get the architecture of the model (random weights), this must match the architecture of the trained model."""
+    architecture = resnet18(weights=None)
+
+    architecture.fc = nn.Sequential(
+        nn.Linear(in_features=512, out_features=256, bias=True),
+        nn.ReLU(),
+        nn.Linear(in_features=256, out_features=6, bias=True),
+    )
+
+    return architecture
+
+
+def load_model() -> ResNet:
+    """Loads the model and adds the trained weights from the .pth file."""
+    download_artifact()
+    model = get_raw_model()
+
+    model_state_dict_path = Path(MODELS_DIR) / MODEL_FILENAME
+    model.load_state_dict(torch.load(model_state_dict_path, map_location="cpu"))
+
+    return model
