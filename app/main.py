dd2cbf7e604bc62f758a3bcdc8135e97ea2544f2
Salman Fariz Navas <46594737+SalmanFarizN@users.noreply.github.com>
2025-09-06 10:18:36 +0000

    Refactor main.py to organize category definitions and improve predict function structure

--- app/main.py
+++ app/main.py
@@ -5,9 +5,22 @@ import io
 from pydantic import BaseModel
 
 from fastapi import FastAPI, File, UploadFile, Depends
+from app.model import load_model, load_transforms
+from torchvision.models import ResNet
+from torchvision.transforms import v2 as transforms
 
 
-# this is a datamodel (datatype, dataclass) that describes the output of the API
+CATEGORIES = [
+    "freshapple",
+    "freshbanana",
+    "freshorange",
+    "rottenapple",
+    "rottenbanana",
+    "rottenorange",
+]
+
+
+# this is a datamodel (datatype same dataclass, but pydantic is implemented in RUST) that describes the output of the API
 class Result(BaseModel):
     category: str
     confidence: float
@@ -21,3 +34,17 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "API is running. Visit /docs for the Swagger API documentation."}
+
+
+# Response model is the Result dataclass defined above
+@app.post("/predict", response_model=Result)
+async def predict(
+    input_image: UploadFile = File(...),  # ... is a placeholder for the file
+    model: ResNet = Depends(
+        load_model
+    ),  # Dependency because it is an async function, will not execute until load_model is done
+    transforms: transforms.Compose = Depends(
+        load_transforms
+    ),  # Dependency because it is an async function, will not execute until load_transforms is done
+):  # missing the return type
+    pass
